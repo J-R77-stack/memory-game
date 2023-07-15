61675449d0e895b438b3f2f95f6d658a6bbd924a
J-R77-stack <johnnyramsay81@gmail.com>
2023-07-15 12:34:38 +0000

    add javascript to remove overlay on click and start game

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+class Memory {
+    constructor(totalTime, circles) {
+        this.circlesArray = circles;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.getElementById("time-remaining");
+        this.ticker = document.getElementById("flips");
+    }
+    startGame(){
+        this.circleToCheck = null;
+        this.totalClicks = 0;
+        this.timeRemaining = this.totalTime;
+        this.matchedCircles = [];
+        this.busy = true;
+    }
+
+    flipCircle(circle) {
+        if(this.canFlipCircle(circle)){
+            this.totalClicks++;
+            this.ticker.innerText = this.totalClicks;
+        }
+    }
+    canFlipCircle(circle) {
+        return true;
+        // return !this.busy && !this.matchedCircles.includes(circle) && card !== this.circleToCheck;
+    }
+}
+
+
+
+function ready() {
+    let overlays = Array.from(document.getElementsByClassName("overlay-text"));
+    let circles = Array.from(document.getElementsByClassName("circle"));
+    let game = new Memory(100, circles);
+
+    overlays.forEach(overlay => {
+        overlay.addEventListener("click", () => {
+            overlay.classList.remove("visible");
+            game.startGame();
+        });
+    });
+    circles.forEach(circle => {
+        circle.addEventListener("click", () => {
+            game.flipCircle(circle)
+        });
+    });
+}
+
+if(document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", ready());
+} else {
+    ready();
+}
